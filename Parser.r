dd0f36ed0b72d8fd99dbd2ad80b4b3bb43d3ccf8
Wei Zhou <wz2372@columbia.edu>
2017-07-17 16:39:30 -0400

    simple parser

---
+++ Parser.r
@@ -0,0 +1,9 @@
+needs(rvest, stringr)
+
+url = "Topic  1. Getting to Know You Discussion Forum.htm"
+page = read_html(url)
+
+texts = page %>%
+  html_nodes("ul.discussion-entries p") 
+
+aaa = texts %>% html_text()
